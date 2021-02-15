cb5ec402c78584c26a2e0d56fc11f062b4b47926
ACLTearr <burroughskaelen@gmail.com>
2021-02-15 18:32:39 -0500

    Ran a ' rake db:migrate ', schema file created with create employees inside of it

--- db/migrate/20210215222854_employees.rb
+++ db/migrate/20210215222854_employees.rb
@@ -1,13 +1,21 @@
 class Employees < ActiveRecord::Migration[5.2]
-  # def change
-  # end
+   #Ask tomorrow if should use change or up/down
+   def change
+      create_table :employees do |t|
+         #  t.column :user_id, :int :limit => 32, :null => false (not sure if needed, docs say id will be created automatically)
+         t.column :first_name, :string
+         t.column :last_name, :string
+         t.column :title, :string
+         #  t.column :email, :string
+      end
+   end
   def self.up
     create_table :employees do |t|
       #  t.column :user_id, :int :limit => 32, :null => false (not sure if needed, docs say id will be created automatically)
        t.column :first_name, :string
        t.column :last_name, :string
        t.column :title, :string
-      #  t.column :email, :string
+      #  t.column :email, :string (not sure if needed, docs dont say email, but list provided has one)
     end
  end
 
