984b11a07ba8144c2757e72f94c8b878531c10d1
Diana dos Santos <diana.dsantos2@gmail.com>
2021-02-15 18:20:27 -0500

    Included Quote table at rails migration format

---
+++ db/migrate/20210215225318_quote.rb
@@ -0,0 +1,15 @@
+class Quote < ActiveRecord::Migration[5.2]
+  def change
+    create_table :quote do |t|
+      t.column :building_type, :string, :limit => 255
+      t.column :nb_app_building, :integer
+      t.column :nb_floors_building, :integer
+      t.column :nb_dist_businesses , :integer
+      t.column :nb_park_spaces , :integer
+      t.column :nb_cages_deployed , :integer
+      t.column :nb_separate_tenant_companies, :integer
+      t.column :nb_occup_floor, :integer
+      t.column :product, :string, :limit => 255
+    end
+  end
+end
