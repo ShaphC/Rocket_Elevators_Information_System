da36634ad20513dd036cc1f10b69cd0747d6c8e8
ShaphanCharles <web_shaph@hotmail.com>
2021-02-17 13:46:11 -0500

    routes correction

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 
-  get 'pages/residential'
-  #root :to => "pages#residential", :id => 'residential'
+  # get 'pages/residential'
+  # #root :to => "pages#residential", :id => 'residential'
 
-  get 'pages/commercial'
+  # get 'pages/commercial'
 
-  get 'pages/quotes'
+  # get 'pages/quotes'
 
-  root 'pages#index'
+  # root 'pages#index'
 
   #get 'pages/:residential', to: 'index#residential', as: 'residential'
   get 'pages/residential' => 'pages#residential', as: :residential
