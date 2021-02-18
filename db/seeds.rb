e5f36623e2f1db1849709a3c9a06a4e235dca527
ACLTearr <burroughskaelen@gmail.com>
2021-02-18 14:43:30 -0500

    Creating users/employees, but not best way, trying to modify

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,43 +6,36 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-employee_list = [
-    [ "Nicolas", "Genest", "CEO", "nicolas.genest@codeboxx.biz" ],
-    [ "Nadya", "Fortier", "Director", "nadya.fortier@codeboxx.biz" ],
-    [ "Martin", "Chantal", "Director Assistant", "martin.chantal@codeboxx.biz" ],
-    [ "Mathieu", "Houde", "Captain", "mathieu.houde@codeboxx.biz" ],
-    [ "David", "Boutin", "Engineer", "david.boutin@codeboxx.biz" ],
-    [ "Mathieu", "Lortie", "Engineer", "mathieu.lortie@codeboxx.biz" ],
-    [ "Thomas", "Carrier", "Engineer", "thomas.carrier@codeboxx.biz" ],
-  ]
-  
-  employee_list.each do |first_name, last_name, title, email|
-    Employee.create(first_name: first_name, last_name: last_name, title: title, email: email)
-  end
+User.create(email: "nicolas.genest@codeboxx.biz", password: 123456, password_confirmation: 123456)
+User.create(email: "nadya.fortier@codeboxx.biz", password: 123456, password_confirmation: 123456)
+User.create(email: "martin.chantal@codeboxx.biz", password: 123456, password_confirmation: 123456)
+User.create(email: "mathieu.houde@codeboxx.biz", password: 123456, password_confirmation: 123456)
+User.create(email: "david.boutin@codeboxx.biz", password: 123456, password_confirmation: 123456)
+User.create(email: "mathieu.lortie@codeboxx.biz", password: 123456, password_confirmation: 123456)
+User.create(email: "thomas.carrier@codeboxx.biz", password: 123456, password_confirmation: 123456)
+User.create(email: "admin@admin.ca", password: 123456, password_confirmation: 123456)
 
-  p "Created #{Employee.count} employee(s)."
+p "Created #{User.count} user(s)."
 
-# Employee.create(first_name: "Nicolas", last_name: "Genest", title: "CEO", email: "nicolas.genest@codeboxx.biz")
-# Employee.create(first_name: "Nadya", last_name: "Fortier", title: "Director", email: "nadya.fortier@codeboxx.biz")
-# Employee.create(first_name: "Martin", last_name: "Chantal", title: "Director Assistant", email: "martin.chantal@codeboxx.biz")
-# Employee.create(first_name: "Mathieu", last_name: "Houde", title: "Captain", email: "mathieu.houde@codeboxx.biz")
-# Employee.create(first_name: "David", last_name: "Boutin", title: "Engineer", email: "david.boutin@codeboxx.biz")
-# Employee.create(first_name: "Mathieu", last_name: "Lortie", title: "Engineer", email: "mathieu.lortie@codeboxx.biz")
-# Employee.create(first_name: "Thomas", last_name: "Carrier", title: "Engineer", email: "thomas.carrier@codeboxx.biz")
+Employee.create!(first_name: "Nicolas", last_name: "Genest", title: "CEO", email: "nicolas.genest@codeboxx.biz", user_id: 1)
+Employee.create(first_name: "Nadya", last_name: "Fortier", title: "Director", email: "nadya.fortier@codeboxx.biz", user_id: 2)
+Employee.create(first_name: "Martin", last_name: "Chantal", title: "Director Assistant", email: "martin.chantal@codeboxx.biz", user_id: 3)
+Employee.create(first_name: "Mathieu", last_name: "Houde", title: "Captain", email: "mathieu.houde@codeboxx.biz", user_id: 4)
+Employee.create(first_name: "David", last_name: "Boutin", title: "Engineer", email: "david.boutin@codeboxx.biz", user_id: 5)
+Employee.create(first_name: "Mathieu", last_name: "Lortie", title: "Engineer", email: "mathieu.lortie@codeboxx.biz", user_id: 6)
+Employee.create(first_name: "Thomas", last_name: "Carrier", title: "Engineer", email: "thomas.carrier@codeboxx.biz", user_id: 7)
+Employee.create(first_name: "Admin", last_name: "Admin", title: "Admin", email: "admin@admin.ca", user_id: 8)
 
-# p "Created #{Employee.count} employee(s)."
+p "Created #{Employee.count} employee(s)."
 
 # Employee.all.each do |employee|
 #   user = User.new({
 #     email: employee.email,
 #     password: 123456,
-#     password_confirmation: 123456,
-#     admin: true})
+#     password_confirmation: 123456})
 #   employee.user = user
 #   employee.save!
 
 #   puts(employee.first_name, employee.user_id)
 #   print
 # end
-
-
