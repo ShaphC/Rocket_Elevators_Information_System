7f11803e18ac6b94bdc69e0fb37030611961ae58
ShaphanCharles <web_shaph@hotmail.com>
2021-02-18 10:51:25 -0500

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -10,37 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_021534) do
-
-  create_table "quote", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "building_type"
-    t.integer "nb_app_building"
-    t.integer "nb_floors_building"
-    t.integer "nb_dist_businesses"
-    t.integer "nb_park_spaces"
-    t.integer "nb_cages_deployed"
-    t.integer "nb_separate_tenant_companies"
-    t.integer "nb_occup_floor"
-    t.string "product"
-  end
-
-  create_table "quotes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "building_type"
-    t.integer "nb_app_building"
-    t.integer "nb_floors_building"
-    t.integer "nb_dist_businesses"
-    t.integer "nb_park_spaces"
-    t.integer "nb_cages_deployed"
-    t.integer "nb_separate_tenant_companies"
-    t.integer "nb_occup_floor"
-    t.string "product"
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "first_name", limit: 35
-    t.string "last_name", limit: 40
-    t.string "email", limit: 250
-    t.string "password", limit: 45
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
