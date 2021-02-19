ec051bbb7ee962d186020c9d754a62dbb51f9076
ShaphanCharles <web_shaph@hotmail.com>
2021-02-19 16:07:23 -0500

    quote delete fix

--- app/controllers/quote_controller.rb
+++ app/controllers/quote_controller.rb
@@ -1,6 +1,7 @@
 class QuoteController < ApplicationController
-    skip_before_action :verify_authenticity_token
-
+    skip_before_filter :verify_authenticity_token 
+    skip_before_filter :authenticate_user!, :only => "reply", :raise => false
+    
     def index
         #@produtos = Produto.order(nome: :desc).limit 2
         #@produto_com_desconto = Produto.order(:preco).limit 1
