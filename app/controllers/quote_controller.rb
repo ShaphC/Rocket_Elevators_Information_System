7599361fb3c6aadb39e6b5c8d98513e8a87ce88c
Diana dos Santos <diana.dsantos2@gmail.com>
2021-02-19 12:43:54 -0500

    correction

--- app/controllers/quote_controller.rb
+++ app/controllers/quote_controller.rb
@@ -1,8 +1,6 @@
 class QuoteController < ApplicationController
 
     def index
-        #@produtos = Produto.order(nome: :desc).limit 2
-        #@produto_com_desconto = Produto.order(:preco).limit 1
     end
 
     def create
