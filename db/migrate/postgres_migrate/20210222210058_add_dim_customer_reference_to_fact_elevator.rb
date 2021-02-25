cf68b288371398e7e826bc0cf53e67aa8dcd0830
ShaphanCharles <web_shaph@hotmail.com>
2021-02-25 12:56:24 -0500

    leads section added

--- db/migrate/postgres_migrate/20210222210058_add_dim_customer_reference_to_fact_elevator.rb
+++
@@ -1,5 +0,0 @@
-class AddDimCustomerReferenceToFactElevator < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :fact_elevators, :dim_customer, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_031216) do
+ActiveRecord::Schema.define(version: 2021_02_24_195423) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -191,11 +191,8 @@ ActiveRecord::Schema.define(version: 2021_02_24_031216) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-<<<<<<< HEAD
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-=======
   add_foreign_key "batteries", "buildings"
->>>>>>> main
   add_foreign_key "building_details", "buildings"
   add_foreign_key "buildings", "addresses"
   add_foreign_key "buildings", "customers"
