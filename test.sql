59b77b27734054f6a32782a183d4ec8f1d7f74d5
ShaphanCharles <web_shaph@hotmail.com>
2021-02-25 18:11:44 -0500

    getting ready to deploy

--- test.sql
+++ test.sql
@@ -3,3 +3,7 @@ SELECT COUNT( [*], [DISTINCT], ContactID) FROM fact_contacts GROUP BY DATE_TRUNC
 SELECT COUNT( [*], [DISTINCT], QuoteId) FROM fact_quotes GROUP BY DATE_TRUNC('month', created_at);
 
 SELECT COUNT( [*], [DISTINCT] SerialNumber) FROM fact_elevators GROUP BY CustomerId, BuildingId;
+
+
+
+SELECT DISTINCT contact_id, created_at from fact_contacts;
