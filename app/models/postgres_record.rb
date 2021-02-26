59517e971957081e642e8725799ff9ff7121daa1
ShaphanCharles <web_shaph@hotmail.com>
2021-02-26 10:50:24 -0500

    getting ready for deployment

--- app/models/postgres_record.rb
+++ app/models/postgres_record.rb
@@ -1,4 +1,4 @@
 class PostgresRecord < ActiveRecord::Base
     self.abstract_class = true
-    establish_connection YAML.load_file(File.join(Rails.root, "config", "postgres_database.yml"))[Rails.env.to_s]
+    ActiveRecord::Base.establish_connection YAML.load_file(File.join(Rails.root, "config", "postgres_database.yml"))[Rails.env.to_s]
   end
